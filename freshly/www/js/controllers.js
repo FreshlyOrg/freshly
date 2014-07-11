230c71d82d7b414d299511de7e62b576a1a2d8a7
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-07-11 12:20:01 -0700

    Added clickable activity buttons

--- freshly/www/js/controllers.js
+++ freshly/www/js/controllers.js
@@ -12,5 +12,17 @@ angular.module('freshly.controllers', [])
 })
 
 .controller('ActivitiesController', function($scope) {
+  $scope.activities = [
+    { name: 'Dodgeball' },
+    { name: 'Wax Museum' },
+    { name: 'Soccer' },
+    { name: 'Segway Tour' },
+    { name: 'Volunteer Boys and Girls Club' },
+    { name: 'Cal Academy Nightlife' },
+    { name: 'Exploratorium Nightlife' },
 
+  ];
+  $scope.showActivity = function(activity) {
+    console.log(activity.name);
+  }
 });
