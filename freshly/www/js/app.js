7f8571882639df3a8b85f683fba2ffb5bd9da4e6
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-07-11 11:56:16 -0700

    Changed app name to freshly

--- freshly/www/js/app.js
+++ freshly/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'freshly.controllers', 'freshly.services'])
+angular.module('freshly', ['ionic', 'freshly.controllers', 'freshly.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
