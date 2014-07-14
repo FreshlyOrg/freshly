d9ef319bcda580d31eade0f6db7f588bc23d65f7
Jacob Adams <jakecadams@gmail.com>
2014-07-14 17:36:51 -0600

    Map loading with from current location, turned off padding

--- freshly/www/app/controllers_views/map/map.js
+++ freshly/www/app/controllers_views/map/map.js
@@ -1,4 +1,6 @@
-angular.module('freshly.map', [])
+angular.module('freshly.map', [
+  'leaflet-directive'
+  ])
 
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
@@ -15,5 +17,30 @@ angular.module('freshly.map', [])
 })
 
 .controller('MapController', function($scope) {
+  
+
+  $scope.location = {
+            zoom: 16,
+            autoDiscover: true
+        };
+
+
+  $scope.getLocation = function(location) {
+
+       var callback = function(position){
+        var newLocation = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+            zoom: 4
+        }
+        location($scope.newLocation);
+      }
+
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(callback);
+      } else { 
+          console.log("Geolocation is not supported by this browser.");
+      }
+  }
 
 });
