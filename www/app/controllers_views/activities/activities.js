ced8b83d64120c86fad23a4919142b393e57d622
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-07-15 14:40:36 -0700

    Updated activity view

--- www/app/controllers_views/activities/activities.js
+++ www/app/controllers_views/activities/activities.js
@@ -15,13 +15,13 @@ angular.module('freshly.activities', [])
 })
 
 .controller('ActivitiesController', function($scope, Activities) {
-  $scope.activities = [];
-  Activities.getActivities().then(function(response) {
-    console.log(response.data);
-    $scope.activities = response.data;
-  }).catch(function(err) {
-    console.log(err);
-  });
+  $scope.refreshActivities = function() {
+    Activities.getActivities().then(function(response) {
+      $scope.activities = response.data;
+    }).catch(function(err) {
+      console.log(err);
+    });
+  };
 
   $scope.toggleActivity = function(activity) {
     if (activity._id === $scope.viewActivity) {
@@ -29,5 +29,40 @@ angular.module('freshly.activities', [])
     } else {
       $scope.viewActivity = activity._id;
     }
-  }
+  };
+
+  $scope.editActivity = function(activity) {
+    $scope.savedActivity = activity;
+    $scope.editing = activity._id;
+  };
+  
+  $scope.cancelEdit = function(activity) {
+    $scope.editing = null;
+    Activities.getActivity(activity._id).then(function(response) {
+      console.log(response);
+    }).catch(function(err) {
+      console.log(err);
+    });
+  };
+
+  $scope.saveEdit = function(activity) {
+    Activity.updateActivity(activity).then(function(response) {
+      console.log(response);
+    }).catch(function(err) {
+      console.log(err);
+    });
+  };
+
+  $scope.deleteActivity = function(activity) {
+    if (confirm("Are you sure you want to delete this activity?")) {
+      Activity.deleteActivity(activity).then(function(response) {
+        console.log(response);
+      }).catch(function(err) {
+        console.log(err);
+      });
+    }
+  };
+
+  $scope.activities = [];
+  $scope.refreshActivities();
 });
