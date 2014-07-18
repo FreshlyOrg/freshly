fb3cc5cf71cf0c032d3cefc58415d057e38b88b3
Jasen Lew <jasen.lew@gmail.com>
2014-07-17 18:04:59 -0700

    Cleans-up code to have activity have both address as well as lat and lng properties

--- www/app/controllers_views/capture/capture.js
+++ www/app/controllers_views/capture/capture.js
@@ -19,36 +19,45 @@ angular.module('freshly.capture', ['geolocation'])
   // Object that holds all activity properties
   $scope.activity = {};
 
-  // Stores current location when go to capture state
-  // $scope.activity.location = GetLocation();
-  $scope.activity.location = "Loading...";
+  $scope.activity.address = "Loading...";
+  $scope.activity.lat = '';
+  $scope.activity.lng = '';
 
   geolocation.getLocation().then(function(data){
-    var lat = $scope.activity.location.latitude = data.coords.latitude;
-    var lng = $scope.activity.location.longitude = data.coords.longitude;
 
+    var lat = data.coords.latitude;
+    var lng = data.coords.longitude;
+    console.log("lat: ", lat);
+    console.log("lng: ", lng);
+
+    // Creates a LatLng object to be passed into reverse geocoder later
     var latlng = new google.maps.LatLng(lat, lng);
+    console.log("latlng object: ", latlng);
 
-    // This is making the Geocode request
+    // Makes a geocode request
     var geocoder = new google.maps.Geocoder();
 
     geocoder.geocode({ 'latLng': latlng }, function (results, status) {
-        if (status !== google.maps.GeocoderStatus.OK) {
-            alert(status);
-        }
-        // This is checking to see if the Geoeode Status is OK before proceeding
-        if (status == google.maps.GeocoderStatus.OK) {
-            console.log(results);
-            var address = (results[0].formatted_address);
-            console.log(address);
+      if (status !== google.maps.GeocoderStatus.OK) {
+        console.log(status);
+      }
 
-            $scope.$apply(function () {
-              $scope.activity.location = address;
-            });
-        }
+      // This is checking to see if the Geoeode Status is OK before proceeding
+      if (status === google.maps.GeocoderStatus.OK) {
+        console.log(results);
+
+        // Grabs the first element's formatted address type - NOTE: Total of 10 elements
+        var address = (results[0].formatted_address);
+        console.log(address);
+
+        // Necessarily uses $apply to update the properties on the $scope
+        $scope.$apply(function () {
+          $scope.activity.address = address;
+          $scope.activity.lat = lat;
+          $scope.activity.lng = lng;
+        });
+      }
     });
-
-    // $scope.activity.location = lat.toString() + ", " + lng.toString();
   });
 
   // Opens camera and allows for photo to be taken and returns photo
@@ -68,10 +77,6 @@ angular.module('freshly.capture', ['geolocation'])
 
   // Send new experience to db and return to app.map state
   $scope.createPin = function () {
-    // JASEN: The two console.logs below were used temporarily to figure out getLocation... can delete.
-    console.log("Latitude: " + $scope.activity.location.latitude);
-    console.log("Longitude: " + $scope.activity.location.longitude);
-
     Activities.addActivity($scope.activity).then(function(response) {
       console.log(response);
       $state.go('app.map');
